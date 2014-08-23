a593b04caafaabd2e9936ca57601a6a92806716e
Matthias Beyer <mail@beyermatthias.de>
2014-08-23 16:58:32 +0200

    Staging: bcm: InterfaceDld.c: Renamed variable: "Adapter" -> "ad"
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- drivers/staging/bcm/InterfaceDld.c
+++ drivers/staging/bcm/InterfaceDld.c
@@ -7,7 +7,7 @@ int InterfaceFileDownload(PVOID arg, struct file *flp, unsigned int on_chip_loc)
 	int errno = 0, len = 0; /* ,is_config_file = 0 */
 	loff_t pos = 0;
 	struct bcm_interface_adapter *intf_ad = arg;
-	/* struct bcm_mini_adapter *Adapter = intf_ad->psAdapter; */
+	/* struct bcm_mini_adapter *ad = intf_ad->psAdapter; */
 	char *buff = kmalloc(MAX_TRANSFER_CTRL_BYTE_USB, GFP_KERNEL);
 
 	if (!buff)
@@ -26,7 +26,7 @@ int InterfaceFileDownload(PVOID arg, struct file *flp, unsigned int on_chip_loc)
 				errno = 0;
 			break;
 		}
-		/* BCM_DEBUG_PRINT_BUFFER(Adapter,DBG_TYPE_INITEXIT, MP_INIT,
+		/* BCM_DEBUG_PRINT_BUFFER(ad, DBG_TYPE_INITEXIT, MP_INIT,
 		 *			  DBG_LVL_ALL, buff,
 		 *			  MAX_TRANSFER_CTRL_BYTE_USB);
 		 */
@@ -113,85 +113,85 @@ exit:
 	return Status;
 }
 
-static int bcm_download_config_file(struct bcm_mini_adapter *Adapter,
+static int bcm_download_config_file(struct bcm_mini_adapter *ad,
 				struct bcm_firmware_info *psFwInfo)
 {
 	int retval = STATUS_SUCCESS;
 	B_UINT32 value = 0;
 
-	if (Adapter->pstargetparams == NULL) {
-		Adapter->pstargetparams =
+	if (ad->pstargetparams == NULL) {
+		ad->pstargetparams =
 			kmalloc(sizeof(struct bcm_target_params), GFP_KERNEL);
-		if (Adapter->pstargetparams == NULL)
+		if (ad->pstargetparams == NULL)
 			return -ENOMEM;
 	}
 
 	if (psFwInfo->u32FirmwareLength != sizeof(struct bcm_target_params))
 		return -EIO;
 
-	retval = copy_from_user(Adapter->pstargetparams,
+	retval = copy_from_user(ad->pstargetparams,
 			psFwInfo->pvMappedFirmwareAddress,
 			psFwInfo->u32FirmwareLength);
 	if (retval) {
-		kfree(Adapter->pstargetparams);
-		Adapter->pstargetparams = NULL;
+		kfree(ad->pstargetparams);
+		ad->pstargetparams = NULL;
 		return -EFAULT;
 	}
 
 	/* Parse the structure and then Download the Firmware */
-	beceem_parse_target_struct(Adapter);
+	beceem_parse_target_struct(ad);
 
 	/* Initializing the NVM. */
-	BcmInitNVM(Adapter);
-	retval = InitLedSettings(Adapter);
+	BcmInitNVM(ad);
+	retval = InitLedSettings(ad);
 
 	if (retval)
 		return retval;
 
-	if (Adapter->LEDInfo.led_thread_running &
+	if (ad->LEDInfo.led_thread_running &
 			BCM_LED_THREAD_RUNNING_ACTIVELY) {
-		Adapter->LEDInfo.bLedInitDone = false;
-		Adapter->DriverState = DRIVER_INIT;
-		wake_up(&Adapter->LEDInfo.notify_led_event);
+		ad->LEDInfo.bLedInitDone = false;
+		ad->DriverState = DRIVER_INIT;
+		wake_up(&ad->LEDInfo.notify_led_event);
 	}
 
-	if (Adapter->LEDInfo.led_thread_running &
+	if (ad->LEDInfo.led_thread_running &
 			BCM_LED_THREAD_RUNNING_ACTIVELY) {
-		Adapter->DriverState = FW_DOWNLOAD;
-		wake_up(&Adapter->LEDInfo.notify_led_event);
+		ad->DriverState = FW_DOWNLOAD;
+		wake_up(&ad->LEDInfo.notify_led_event);
 	}
 
 	/* Initialize the DDR Controller */
-	retval = ddr_init(Adapter);
+	retval = ddr_init(ad);
 	if (retval)
 		return retval;
 
 	value = 0;
-	wrmalt(Adapter, EEPROM_CAL_DATA_INTERNAL_LOC - 4,
+	wrmalt(ad, EEPROM_CAL_DATA_INTERNAL_LOC - 4,
 				&value, sizeof(value));
-	wrmalt(Adapter, EEPROM_CAL_DATA_INTERNAL_LOC - 8,
+	wrmalt(ad, EEPROM_CAL_DATA_INTERNAL_LOC - 8,
 				&value, sizeof(value));
 
-	if (Adapter->eNVMType == NVM_FLASH) {
-		retval = PropagateCalParamsFromFlashToMemory(Adapter);
+	if (ad->eNVMType == NVM_FLASH) {
+		retval = PropagateCalParamsFromFlashToMemory(ad);
 		if (retval)
 			return retval;
 	}
 
-	retval = buffDnldVerify(Adapter, (PUCHAR)Adapter->pstargetparams,
+	retval = buffDnldVerify(ad, (PUCHAR)ad->pstargetparams,
 			sizeof(struct bcm_target_params), CONFIG_BEGIN_ADDR);
 
 	if (retval)
-		BCM_DEBUG_PRINT(Adapter, DBG_TYPE_INITEXIT,
+		BCM_DEBUG_PRINT(ad, DBG_TYPE_INITEXIT,
 				MP_INIT, DBG_LVL_ALL,
 				"configuration file not downloaded properly");
 	else
-		Adapter->bCfgDownloaded = TRUE;
+		ad->bCfgDownloaded = TRUE;
 
 	return retval;
 }
 
-int bcm_ioctl_fw_download(struct bcm_mini_adapter *Adapter,
+int bcm_ioctl_fw_download(struct bcm_mini_adapter *ad,
 			struct bcm_firmware_info *psFwInfo)
 {
 	int retval = STATUS_SUCCESS;
@@ -201,8 +201,8 @@ int bcm_ioctl_fw_download(struct bcm_mini_adapter *Adapter,
 	 * Firmware. Check for the Config file to be first to be sent from the
 	 * Application
 	 */
-	atomic_set(&Adapter->uiMBupdate, false);
-	if (!Adapter->bCfgDownloaded &&
+	atomic_set(&ad->uiMBupdate, false);
+	if (!ad->bCfgDownloaded &&
 		psFwInfo->u32StartingAddress != CONFIG_BEGIN_ADDR) {
 		/* Can't Download Firmware. */
 		return -EINVAL;
@@ -210,7 +210,7 @@ int bcm_ioctl_fw_download(struct bcm_mini_adapter *Adapter,
 
 	/* If Config File, Finish the DDR Settings and then Download CFG File */
 	if (psFwInfo->u32StartingAddress == CONFIG_BEGIN_ADDR) {
-		retval = bcm_download_config_file(Adapter, psFwInfo);
+		retval = bcm_download_config_file(ad, psFwInfo);
 	} else {
 		buff = kzalloc(psFwInfo->u32FirmwareLength, GFP_KERNEL);
 		if (buff == NULL)
@@ -224,7 +224,7 @@ int bcm_ioctl_fw_download(struct bcm_mini_adapter *Adapter,
 			goto error;
 		}
 
-		retval = buffDnldVerify(Adapter,
+		retval = buffDnldVerify(ad,
 					buff,
 					psFwInfo->u32FirmwareLength,
 					psFwInfo->u32StartingAddress);
@@ -238,7 +238,7 @@ error:
 	return retval;
 }
 
-static INT buffDnld(struct bcm_mini_adapter *Adapter,
+static INT buffDnld(struct bcm_mini_adapter *ad,
 			PUCHAR mappedbuffer, UINT u32FirmwareLength,
 			ULONG u32StartingAddress)
 {
@@ -249,7 +249,7 @@ static INT buffDnld(struct bcm_mini_adapter *Adapter,
 
 	while (u32FirmwareLength) {
 		len = MIN_VAL(u32FirmwareLength, MAX_TRANSFER_CTRL_BYTE_USB);
-		retval = wrm(Adapter, u32StartingAddress, mappedbuffer, len);
+		retval = wrm(ad, u32StartingAddress, mappedbuffer, len);
 
 		if (retval)
 			break;
@@ -260,7 +260,7 @@ static INT buffDnld(struct bcm_mini_adapter *Adapter,
 	return retval;
 }
 
-static INT buffRdbkVerify(struct bcm_mini_adapter *Adapter,
+static INT buffRdbkVerify(struct bcm_mini_adapter *ad,
 			PUCHAR mappedbuffer, UINT u32FirmwareLength,
 			ULONG u32StartingAddress)
 {
@@ -274,7 +274,7 @@ static INT buffRdbkVerify(struct bcm_mini_adapter *Adapter,
 
 	while (u32FirmwareLength && !retval) {
 		len = MIN_VAL(u32FirmwareLength, MAX_TRANSFER_CTRL_BYTE_USB);
-		bytes = rdm(Adapter, u32StartingAddress, readbackbuff, len);
+		bytes = rdm(ad, u32StartingAddress, readbackbuff, len);
 
 		if (bytes < 0) {
 			retval = bytes;
@@ -296,19 +296,19 @@ static INT buffRdbkVerify(struct bcm_mini_adapter *Adapter,
 	return retval;
 }
 
-INT buffDnldVerify(struct bcm_mini_adapter *Adapter,
+INT buffDnldVerify(struct bcm_mini_adapter *ad,
 			unsigned char *mappedbuffer,
 			unsigned int u32FirmwareLength,
 			unsigned long u32StartingAddress)
 {
 	INT status = STATUS_SUCCESS;
 
-	status = buffDnld(Adapter, mappedbuffer,
+	status = buffDnld(ad, mappedbuffer,
 			u32FirmwareLength, u32StartingAddress);
 	if (status != STATUS_SUCCESS)
 		goto error;
 
-	status = buffRdbkVerify(Adapter, mappedbuffer,
+	status = buffRdbkVerify(ad, mappedbuffer,
 			u32FirmwareLength, u32StartingAddress);
 	if (status != STATUS_SUCCESS)
 		goto error;
